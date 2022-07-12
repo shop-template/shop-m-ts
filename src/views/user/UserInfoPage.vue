98c9583ef64842fcc1f0ae27aa276405b8623474
fxss <723107234@qq.com>
2022-07-12 22:05:28 +0800

    feat: :sparkles: 个人信息头像单图上传

--- src/views/user/UserInfoPage.vue
+++ src/views/user/UserInfoPage.vue
@@ -43,7 +43,6 @@ import { Toast } from 'vant'
 import type { UploaderInstance, UploaderFileListItem } from 'vant'
 import Compressor from 'compressorjs'
 import { useUserStore } from '@/store'
-import { getType } from '@/utils'
 
 const router = useRouter()
 const route = useRoute()
@@ -77,88 +76,42 @@ function choseUserImgEvent() {
   userImgUploader.value?.chooseFile()
 }
 
-let chooseFileLength = 0
-function userImgBeforeRead(
-  file: File | File[],
-  detail: {
-    name: number | string
-    index: number
-  },
-): Promise<File | File[]> {
-  const toast = Toast('文件压缩中...')
+function userImgBeforeRead(file: File | File[]): Promise<File> {
+  const toast = Toast.loading({
+    message: '文件压缩中...',
+  })
   return new Promise((resolve) => {
     // compressorjs 默认开启 checkOrientation 选项、图片压缩
-    if (getType(file) === 'Array') {
-      const curFile = file as File[]
-      chooseFileLength = curFile?.length
-      const filePromiseList = curFile?.map((x) => {
-        return new Promise((resolve) => {
-          // eslint-disable-next-line no-new
-          new Compressor(x, {
-            success: resolve,
-            error(err) {
-              console.log(err.message)
-            },
-          })
-        })
-      })
-      Promise.all(filePromiseList).then((res) => {
+    const curFile = file as File
+    // eslint-disable-next-line no-new
+    new Compressor(curFile, {
+      success: (res) => {
         toast.clear()
-        resolve(res as File[])
-      })
-    } else {
-      const curFile = file as File
-      chooseFileLength = 0
-      // eslint-disable-next-line no-new
-      new Compressor(curFile, {
-        success: (res) => {
-          toast.clear()
-          resolve(res as File)
-        },
-        error(err) {
-          console.log(err.message)
-        },
-      })
-    }
+        resolve(res as File)
+      },
+      error(err) {
+        console.log(err.message)
+      },
+    })
   })
 }
 
 function userImgOversize(file: UploaderFileListItem | UploaderFileListItem[]) {
-  if (getType(file) === 'Array') {
-    const curFile = file as UploaderFileListItem[]
-    const nameList = curFile.map((x) => x?.file?.name)
-    Toast(`${nameList.join('、')}文件大小不能超过 5M`)
-  } else {
-    Toast('文件大小不能超过 5M')
-  }
+  Toast('文件大小不能超过 5M')
 }
 function userImgAfterRead(file: UploaderFileListItem | UploaderFileListItem[]) {
   console.log(file)
   const formData = new FormData()
-  if (getType(file) === 'Array') {
-    const curFile = file as UploaderFileListItem[]
-    if (curFile.length !== chooseFileLength) return
-    curFile.forEach((element, index) => {
-      formData.append(`file${index}`, element?.file as File)
-    })
-  } else {
-    const curFile = file as UploaderFileListItem
-    formData.append('file', curFile?.file as File)
-  }
+  const curFile = file as UploaderFileListItem
+  formData.append('file', curFile?.file as File)
 
   // 自行处理上传逻辑
   const toast = Toast.loading({
-    message: '上传中...',
+    message: '文件上传中...',
   })
   setTimeout(() => {
     toast.clear()
-    if (getType(file) === 'Array') {
-      const curFile = file as UploaderFileListItem[]
-      userStore.userInfo.headerImg = curFile[0].content
-    } else {
-      const curFile = file as UploaderFileListItem
-      userStore.userInfo.headerImg = curFile.content
-    }
+    userStore.userInfo.headerImg = curFile.content
   }, 1000)
 }
 
